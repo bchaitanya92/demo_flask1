9a4dcac0c3e4e022c25a31c8b33e82b2329bf257
Bourisetti Chaitanya <140358296+bchaitanya92@users.noreply.github.com>
2024-11-30 07:49:01 +0530

    Update blog.py

--- models/blog.py
+++ models/blog.py
@@ -1,14 +1,14 @@
 from utils.db import db
 
 class Author(db.Model):
-    id = db.Column(db.Integer, primary_key=True)  # Primary key for Author
-    name = db.Column(db.String(100), nullable=False)  # Name of the author
-    email = db.Column(db.String(100), unique=True, nullable=False)  # Unique email for each author
-    blogs = db.relationship('Blog', backref='author', lazy=True)  # Relationship to Blog table
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
 
 class Blog(db.Model):
-    id = db.Column(db.Integer, primary_key=True)  # Primary key for Blog
-    title = db.Column(db.String(100), nullable=False)  # Blog title
-    content = db.Column(db.String(5000), nullable=False)  # Blog content
-    date = db.Column(db.String(100), nullable=False)  # Date of blog creation
-    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)  # Foreign key to Author
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.String(5000), nullable=False)
+    date = db.Column(db.String(100), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
+    author = db.relationship('Author', backref=db.backref('blogs', lazy=True))
+
