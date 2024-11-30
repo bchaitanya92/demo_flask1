b8abe947b9c624006c562fb1824a1a445ddc5440
Bourisetti Chaitanya <140358296+bchaitanya92@users.noreply.github.com>
2024-11-30 07:35:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,29 +20,25 @@ def about():
 
 @flask_app.route('/blogs')
 def blogs():
-    # Fetch all blogs from the database
     blogs_list = Blog.query.all()
     return render_template('blogs.html', blogs=blogs_list)
 
 @flask_app.route('/add_blog', methods=['GET', 'POST'])
 def add_blog():
     if request.method == 'POST':
-        # Fetch form data
         author_name = request.form['author_name']
         author_email = request.form['author_email']
         blog_title = request.form['blog_title']
         blog_content = request.form['blog_content']
         blog_date = request.form['blog_date']
 
-        # Check if the author exists
+
         author = Author.query.filter_by(email=author_email).first()
         if not author:
-            # If author does not exist, create a new one
             author = Author(name=author_name, email=author_email)
             db.session.add(author)
             db.session.commit()
 
-        # Add the blog
         new_blog = Blog(
             title=blog_title,
             content=blog_content,
